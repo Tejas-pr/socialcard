60edbc308d7aa8775f1d16224be087f1842db21c
Tejas-pr <Tejas.teju02@gmail.com>
2024-12-16 13:42:15 +0530

    added showcard component

--- frontend/src/pages/ShowCard.tsx
+++ frontend/src/pages/ShowCard.tsx
@@ -1,10 +1,11 @@
+import TiltCard from "@/components/Tiltcard";
 
 const ShowCard = () => {
   return (
-    <div>
-      show cards
+    <div className="grid h-screen w-full place-content-center bg-gradient-to-br from-indigo-500 to-violet-500 px-4 py-12 text-slate-900">
+      <TiltCard />
     </div>
-  )
-}
+  );
+};
 
-export default ShowCard
+export default ShowCard;
